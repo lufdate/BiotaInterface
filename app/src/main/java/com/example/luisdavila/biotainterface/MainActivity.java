fae4be3024d036a5685777d60b587988a1ea8e1c
luis.davila <lufdate@gmail.com>
2018-07-03 10:25:41 -0500

    First commit

--- app/src/main/java/com/example/luisdavila/biotainterface/MainActivity.java
+++ app/src/main/java/com/example/luisdavila/biotainterface/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    // prueba commit
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
